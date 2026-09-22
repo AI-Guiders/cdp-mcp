2db06bf9abdacf3ea2b2aa512bf04594cdf859ff
Каратаев Дмитрий <dkarataev1990@gmail.com>
2026-09-22 17:51:02 +0300

    Register sql dialect backends in CDP LRC host.
    Wire language.sql, sql.postgres, and sql.mssql backends with generic fallback last.

--- CdpLanguageResolverHost.cs
+++ CdpLanguageResolverHost.cs
@@ -1,5 +1,6 @@
 using AIGuiders.Platform.Execution.Language;
 using AIGuiders.Platform.Language.CSharp;
+using AIGuiders.Platform.Language.Sql;
 using AIGuiders.Platform.Modeling.Language.Adapters.Fcs;
 using AIGuiders.Platform.Modeling.Language.Adapters.Gdl;
 using DashSpec.Modeling.Language.Adapters.DashSpec;
@@ -19,6 +20,9 @@ internal static class CdpLanguageResolverHost
             .Register(new FcsLanguageBackend(null))
             .Register(new GdlLanguageBackend())
             .Register(new CsharpLanguageBackend())
+            .Register(new SqlPostgresLanguageBackend())
+            .Register(new SqlMssqlLanguageBackend())
+            .Register(new SqlLanguageBackend())
             .Register(new DashSpecLanguageBackend())
             .Build();
 }
